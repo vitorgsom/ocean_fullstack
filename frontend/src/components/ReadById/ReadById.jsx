10a8b18d35ae60064edfb5836ad376b18e5e7b1b
vitorgsom <vitorgabrieltitos@gmail.com>
2023-02-24 11:36:34 -0400

    Implementação ReadById

--- frontend/src/components/ReadById/ReadById.jsx
+++ frontend/src/components/ReadById/ReadById.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from '../Card/Card'
 
 export default function ReadById() {
+  const [item, setItem] = useState()
+
+  const id = "63f773c956d46e126a4dcb78"
+
+  async function realizarRequisicao() {
+    const url = "http://localhost:3000/item/" + id
+    // const url = "https://ocean-fullstack.onrender.com/item"
+  
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setItem(data)
+  }
+
+  useEffect( () => {
+    realizarRequisicao()
+  }, [])
+
+  if (!item) {
+    return <div>Carregando . . .</div>
+  }
+
   return (
-    <div>ReadById</div>
+    <div className="ReadById">
+      <Card item={item} />
+    </div>
   )
+
 }
