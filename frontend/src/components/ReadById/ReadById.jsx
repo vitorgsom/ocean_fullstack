9a37bfb8b1e5992bc0ce682a1e04da6aca4b4e8e
Vitor Melo <104541326+vitorgsom@users.noreply.github.com>
2023-02-24 11:56:45 -0400

    Detalhe final

--- frontend/src/components/ReadById/ReadById.jsx
+++ frontend/src/components/ReadById/ReadById.jsx
@@ -7,8 +7,8 @@ export default function ReadById() {
   const id = "63f773c956d46e126a4dcb78"
 
   async function realizarRequisicao() {
-    const url = "http://localhost:3000/item/" + id
-    // const url = "https://ocean-fullstack.onrender.com/item"
+    // const url = "http://localhost:3000/item/" + id
+    const url = "https://ocean-fullstack.onrender.com/item"
   
     const response = await fetch(url)
     const data = await response.json()
